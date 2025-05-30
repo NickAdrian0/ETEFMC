b4fc5e2d9f3d737a548b0edd16980f7bf410061a
NickAdrian0 <nicolaspadriano@gmail.com>
2025-05-30 09:34:46 -0300

    Tarefa1_Aula7_LPR

---
+++ LinguagemDeProgramacao/Tarefas_LPR/Aula7/Tarefa1_C#/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] numInt = new int[10];
+int[] Par = new int[10];
+int[] Impar = new int[10];
+
+    Console.WriteLine("Bem vindo ao separador de numeros. Insira 10 números e lhe direi se são pares ou impares.");
+for (int i = 0; i < numInt.Length; i++) {
+    Console.WriteLine("Insira o " + (i+1) + "o número");
+    numInt[i] = Convert.ToInt32(Console.ReadLine());
+    if (numInt[i] % 2 == 0) Par[i] = numInt[i]; else Impar[i] = numInt[i];
+}
+Console.Write("Par:");
+foreach(int i in Par) if(i != 0) Console.Write(" " + i);
+Console.Write("\nImpar:");
+foreach(int i in Impar) if(i != 0) Console.Write(" " + i); 
